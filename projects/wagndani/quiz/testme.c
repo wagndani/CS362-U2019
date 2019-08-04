ab9e7d8983fd7bc60036b0d4fbaa05db11f5a003
Daniel Wagner <wagndani@flip1.engr.oregonstate.edu>
2019-08-04 12:34:17 -0700

    Random quiz submittal

---
+++ projects/wagndani/quiz/testme.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+    // Keeping return within the range of printable standard ascii characters
+    char ret = (rand() % 95) + 32;
+    
+    // Pick from entire set of ascii characters
+    //char ret = rand() % 256;
+    
+    return ret;
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+    int len = 6;
+    char * retString;
+    int i;
+    char reset[] = {'r', 'e', 's', 't'};
+
+    retString = calloc((size_t)len, sizeof(char));
+    
+    for(i = 0; i < len - 1; i++){
+        
+        retString[i] = reset[(rand() % 4)];
+    }
+    return retString;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      free(s);
+      printf("error ");
+      exit(200);
+    }
+    free(s);
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
